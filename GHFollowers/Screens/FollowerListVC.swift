c7b352e48c8ade107fd715a95b70c86523643ae2
Akadet Mathurapoj <akadet.m@linecorp.com>
2020-01-19 09:13:25 +0700

    organize project structure

---
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -0,0 +1,22 @@
+//
+//  FollowerListVC.swift
+//  GHFollowers
+//
+//  Created by Akadet on 1/15/20.
+//  Copyright © 2020 $(PRODUCT_BUNDLE_IDENTIFIER). All rights reserved.
+//
+
+import UIKit
+
+class FollowerListVC: UIViewController {
+
+    var username: String!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .white
+        navigationController?.isNavigationBarHidden = false
+    }
+
+}
